ffb8d0c54a85b5bfd263c56ef3a9d0414b1c19f1
Riva Kansakar <rivakansakar21@augustana.edu>
2024-03-02 12:35:08 -0600

    Added body is profile

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 
 class MyProfileScreen extends StatelessWidget {
-  const MyProfileScreen({super.key});
+  const MyProfileScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    const double defaultPadding = 16.0;
     return Scaffold(
       appBar: AppBar(
-
+        title: Text(
+          "Profile",
+          style: Theme.of(context).textTheme.headline6,
+        ),
       ),
-      body: const Center(
-        child: Text('Welcome to the profile screen!'),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(defaultPadding),
+          child: const Column(
+            children: [
+              SizedBox(
+                width: 190, height: 120,
+                //child: Image (image: AssetImage('lib/assets/images/user_profile.jpg')),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
