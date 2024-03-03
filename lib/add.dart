55eea226b266dc9fef50f891e03b984f06287aaa
Riva Kansakar <rivakansakar21@augustana.edu>
2024-03-02 19:10:35 -0600

    Completed add page

--- lib/add.dart
+++ lib/add.dart
@@ -30,106 +30,126 @@ class _MyAddScreenState extends State<MyAddScreen> {
     const double defaultPadding = 16.0;
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0), // Set the desired margin value
-        child: Column(
-          children: [
-            const Text(
-              "Food Name",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0), // Set the desired margin value
+          child: Column(
+            children: [
+              const Text(
+                "Food Name",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            const TextField(
-             
-              decoration: InputDecoration(
-                hintText: "Enter food name",
-              ),
-            ),
-            const SizedBox(height: defaultPadding), // Add a SizedBox for spacing
-            const Text(
-              "Expiry Date",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            InkWell(
-              onTap: () {
-                _selectDate(context);
-              },
-              child: InputDecorator(
+              const TextField(
                 decoration: InputDecoration(
-                  hintText: "Select expiry date",
-                ),
-                child: Text(
-                  _selectedDate != null
-                      ? "${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}"
-                      : "Select expiry date",
+                  hintText: "Enter food name",
                 ),
               ),
-            ),
-            const SizedBox(height: defaultPadding), // Add a SizedBox for spacing
-            const Text(
-              "Category",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+              const SizedBox(height: defaultPadding), // Add a SizedBox for spacing
+              const Text(
+                "Expiry Date",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-             DropdownButton(
-              isExpanded: true,               
-              items: const <String>['Fruit', 'Vegetable', 'Meat', 'Dairy', 'Bakery', 'Pantry', 'Frozen', 'Beverage', 'Others']
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-              onChanged: (_) {},
-            ),  
-            const SizedBox(height: defaultPadding), // Add a SizedBox for spacing
-            const Text(
-              "Set Reminder",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-             DropdownButton(
-              isExpanded: true,               
-              items: const <String>['1 day before', '2 days before', '5 days before', 'A week before', 'A month before']
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-              onChanged: (_) {},
-            ),   
-        
-   
-            Container(
-              width: 100,
-              child: FloatingActionButton(
-                
-                onPressed: () {
-                  // Put your code here to be executed when the FAB is pressed.
+              InkWell(
+                onTap: () {
+                  _selectDate(context);
                 },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    
-                    Icon(Icons.add), // Add leading add icon here
-                    Text("Add"),
-                  ],
+                child: InputDecorator(
+                  decoration: InputDecoration(
+                    hintText: "Select expiry date",
+                  ),
+                  child: Text(
+                    _selectedDate != null
+                        ? "${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}"
+                        : "Select expiry date",
+                  ),
                 ),
-                
-                
               ),
-            )       
-          ],
+              const SizedBox(height: defaultPadding), // Add a SizedBox for spacing
+              const Text(
+                "Category",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              DropdownButton(
+                isExpanded: true,
+                items: const <String>[
+                  'Fruit',
+                  'Vegetable',
+                  'Meat',
+                  'Dairy',
+                  'Bakery',
+                  'Pantry',
+                  'Frozen',
+                  'Beverage',
+                  'Others'
+                ].map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (_) {},
+              ),
+              const SizedBox(height: defaultPadding), // Add a SizedBox for spacing
+              const Text(
+                "Set Reminder",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              DropdownButton(
+                isExpanded: true,
+                items: const <String>[
+                  '1 day before',
+                  '2 days before',
+                  '5 days before',
+                  'A week before',
+                  'A month before'
+                ].map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (_) {},
+              ),
+              const SizedBox(height: defaultPadding), // Add a SizedBox for spacing
+              Container(
+                  width: 400,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: const Center(
+                    child: Icon(Icons.camera_alt),
+                  )),
+              const SizedBox(height: defaultPadding), // Add a SizedBox for spacing
+              Container(
+                width: 100,
+                child: FloatingActionButton(
+                  onPressed: () {
+                    // Put your code here to be executed when the FAB is pressed.
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.add), // Add leading add icon here
+                      Text("Add"),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
